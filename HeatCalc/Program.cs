ff4aa5f4200ed9486dd6f70e37f154ccb7128be7
Yuliya <julya.kolosova92@yandex.ru>
2020-07-22 21:49:31 +0700

    Приведена в порядок. Написана без ООП, обычный код для простоты объяснения физики.

--- HeatCalc/Program.cs
+++ HeatCalc/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HeatCalc
 {
@@ -11,97 +9,103 @@ namespace HeatCalc
     {
         static void Main(string[] args)
         {
-            double eps = 0.1;
 
-            double N = 100;
-            double N1 = 50;
+            System.IO.StreamWriter File1 = new StreamWriter("T(x)" + ".txt");
+            string head = "x[i]" + "     " + "T_new[i]";
+            File1.WriteLine(head);
+            System.IO.StreamWriter File2 = new StreamWriter("time(eps)" + ".txt");
+            string head1 = "time" + "     " + "eps";
+            File2.WriteLine(head1);
 
-            double l1 = 1;
-            double l2 = 1;
-            double L = L
-            double ro1 = 7800;
-            double ro2 = 7800;
-            double lyambda_1 = 38.5;
-            double lyambda_2 = 38.5;
-            double C_1 = 380;
-            double C_2 = 380;
-            
-            K = N1;
-            L = l1 + l2;
-            h = L / N;
-            H = 5;
-            T_left = 100;
-            T_right = 25;
-            T_air = 25;
 
-            double[] T_old = new double[N + 1];
-            double[] T_new = new double[N + 1];
-            double[] X = new double[N + 1];
+            double eps = 0.1; // погрешность
+            double r = 0.2; // число Куранта
+            double time = 0; // старт времени
+            double dT = 1; // разница температуры между старым и новым слоем по времени
+                           // (нужно только вначале, чтобы не было по умолчанию 0)
+
+            int N = 100; // Количество разбиений
+            int K = 50; // Место соединения двух материалов
+
+            double l1 = 0.5; // Длина 1 материала
+            double l2 = 0.5; // Длина 2 материала
+            double L = l1 + l2; // Общая длина материалов
+            double h = L / N; // Шаг по пространству
+            double tau = h * h * r; // Шаг по времени
+
+            double ro1 = 7800; // Плотность 1 материала
+            double ro2 = 7800; // Плотность 2 материала
+            double lyamb1 = 38.5; // Теплопроводность 1 материала
+            double lyamb2 = 38.5; // Теплопроволность 2 материала
+            double C1 = 380; // Теплоемкость 1 материала
+            double C2 = 380; // Теплоемкость 2 материала
+            double a1 = lyamb1 / (C1 * ro1); // Коэффициент температуропроводности 1 материала
+            double a2 = lyamb2 / (C2 * ro2); // Коэффициент температуропроводности 2 материала
+
+            double TLeft = 100; // Температура на левой границе 1 материала - для 1 ГУ
+            double Tl1 = 100; // Начальная температура 1 материала
+            double Tl2 = 25; // Начальная температура 2 материала
+
+            double[] TOld = new double[N + 1]; // Массив данных старого слоя температуры
+            double[] TNew = new double[N + 1]; // Массив данных нового слоя температуры
+            double[] X = new double[N + 1]; // Данные по оси Х
 
             for (int i = 0; i <= N; i++)
             {
-                X[i] = i * h;
+                X[i] = i * h; //  Разбиение оси Х на шаги
             }
 
-            T_old[0] = T_left;
+            TOld[0] = TLeft; // Первое граничное условие (постоянство температуры)
+
             for (int i = 1; i <= N; i++)
             {
                 if (i <= K)
-                    T_old[i] = T_left;
+                    TOld[i] = Tl1; // Распределение начальной температуры на 1 материале
                 if (i > K)
-                    T_old[i] = T_right;
+                    TOld[i] = Tl2; // Распределение начальной температуры на 2 материале
             }
-
-            r = 0.2;
-            tau = 1;// h * h * r;
-
-            time = 0;
-            dT = 1;
+            for (int i = 0; i <= N; i++)
+            {
+                string X_T = X[i].ToString().Replace(",", ".") + "\t" + "    " + TOld[i].ToString().Replace(",", ".") + "\t";
+                File1.WriteLine(X_T);
+                Console.WriteLine(X[i].ToString().Replace(",", ".") + "\t" + "    " + TOld[i].ToString().Replace(",", ".") + "\t");
+            }
             while (dT > eps)
             {
-                time = time + tau;
-                T_new[0] = T_old[0];
+                time += tau; // увеличиваем время на tau
+                TNew[0] = TOld[0]; // 1 граничное условие (постоянство температуры слева)
+
                 for (int i = 1; i < K; i++)
                 {
-                    T_new[i] = T_old[i] + lyambda_1 / (C_1 * Ro_1) * tau * (T_old[i - 1] - 2 * T_old[i]
-                        + T_old[i + 1]) / (h * h);
+                    TNew[i] = TOld[i] + a1 * tau / (h * h) * (TOld[i - 1] - 2 * TOld[i] + TOld[i + 1]); // Температура на 1 материале
                 }
 
-                T_new[K] = (lyambda_2 * T_old[K + 1] + lyambda_1 * T_old[K - 1]) / (lyambda_2 + lyambda_1);
+                TNew[K] = (lyamb2 * TOld[K + 1] + lyamb1 * TOld[K - 1]) / (lyamb2 + lyamb1); // Температура на соединении
 
                 for (int i = K + 1; i < N; i++)
                 {
-                    T_new[i] = T_old[i] + lyambda_2 / (C_2 * Ro_2) * tau *
-                        (T_old[i - 1] - 2 * T_old[i] + T_old[i + 1]) / (h * h);
+                    TNew[i] = TOld[i] + a2 * tau / (h * h) * (TOld[i - 1] - 2 * TOld[i] + TOld[i + 1]); // Температура на 2 материале
                 }
-                T_new[N] = T_new[N - 1];
-                //H * h * (T_old[N] - T_air) / lyambda_2 + T_old[N - 1];//коэфф теплоотдачи
 
-                dT = Math.Abs(T_old[N1 + 1] - T_new[N1 + 1]);
+                TNew[N] = TNew[N - 1]; // Условие симметрии на конце материала 2 
 
-                T_old_Sum = T_old.Sum();
-                T_new_Sum = T_new.Sum();
-                //dT = Math.Abs(T_old_Sum - T_new_Sum);
+                dT = Math.Abs(TOld[K] - TNew[K]);
 
                 for (int i = 0; i <= N; i++)
                 {
-                    T_old[i] = T_new[i];
+                    TOld[i] = TNew[i];
 
-                    string X_T = X[i].ToString().Replace(",", ".") + "\t" + "    " + T_new[i].ToString().Replace(",", ".") + "\t";
+                    string X_T = X[i].ToString().Replace(",", ".") + "\t" + "    " + TNew[i].ToString().Replace(",", ".") + "\t";
                     File1.WriteLine(X_T);
+                    Console.WriteLine(X[i].ToString().Replace(",", ".") + "\t" + "    " + TNew[i].ToString().Replace(",", ".") + "\t");
+
                 }
                 string Time_eps = time.ToString().Replace(",", ".") + "\t" + "    " + dT.ToString().Replace(",", ".") + "\t";
                 File2.WriteLine(Time_eps);
             }
             File1.Close();
             File2.Close();
-            var Gran = new List<T_x>();
-            for (int i = 0; i <= N; i++)
-            {
-                Gran.Add(new T_x(Math.Round(X[i], 4), Math.Round(T_new[i], 4)));
-            }
-            this.tablegrid_xy.ItemsSource = Gran;
+            Console.ReadKey();
         }
-    }
     }
 }
